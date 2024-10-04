8b8a965f43b469759b6be501eb6d9cb4105901b2
zheng <hello@zheng.cn>
2024-10-04 13:33:53 +0800

    完善文件上传流程

--- src/main/java/com/zheng/controller/FileController.java
+++ src/main/java/com/zheng/controller/FileController.java
@@ -8,18 +8,13 @@ import com.zheng.pojo.File;
 import com.zheng.pojo.Result;
 import com.zheng.pojo.vo.FileVo;
 import com.zheng.service.FileService;
-import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
 import io.minio.RemoveObjectArgs;
-import io.minio.errors.*;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -30,6 +25,12 @@ public class FileController {
     private MinioClient minioClient;
     @Autowired
     private FileService fileService;
+    private String[] video = {"mp4", "avi", "rmvb", "mkv", "mov", "MP4", "AVI", "RMVB", "MKV", "MOV"};
+    private String[] music = {"mp3", "wav", "wma", "mp2", "flac", "midi", "ra", "ape", "AAC", "CDA", "MP3", "WAV", "WMA", "MP2", "FLAC", "MIDI", "RA", "APE", "AAC", "CDA"};
+    private String[] image = {"jpeg", "jpg", "png", "gif", "bmp", "dds", "psd", "pdt", "webp", "xmp", "svg", "tiff", "JPEG", "JPG", "PNG", "GIF", "BMP", "DDS", "PSD", "PDT", "WEBP", "XMP", "SVG", "TIFF"};
+    private String[] pdf = {"PDF", "PPTX", "pdf", "pptx"};
+    private String[] word = {"DOCX", "docx"};
+    private String[] excel = {"XLSX", "xlsx"};
     @PostMapping("createfolder")
     public Result CreateFolder(@RequestBody FileVo fileVo){
         File file = new File();
@@ -77,6 +78,8 @@ public class FileController {
     }
     @PostMapping("checkfile")
     public Result checkFile(String fileMd5,String Filename,Long FileSize,String pid,String userId){
+        String extName = FileUtil.extName(Filename);
+        System.out.println(extName);
         LambdaQueryWrapper<File> fileLambdaQueryWrapper = new LambdaQueryWrapper<>();
         fileLambdaQueryWrapper.eq(File::getFileMd5,fileMd5);
         File one = fileService.getOne(fileLambdaQueryWrapper);
@@ -91,7 +94,7 @@ public class FileController {
             file1.setFileSize(FileSize);
             file1.setFolderType(1);
             file1.setPId(pid);
-            file1.setFileCategory(1);
+            filesetcategory(file1,extName);
             file1.setFileMd5(fileMd5);
             file1.setStatus(1);
             file1.setUpdatetime(time);
@@ -100,6 +103,7 @@ public class FileController {
             }else {
                 file1.setFilePath(pid+"/"+Filename);
             }
+
             fileService.save(file1);
             return Result.ok("文件尚未上传");
         }
@@ -123,5 +127,25 @@ public class FileController {
             fileList.add(fileVo1);
         });
         return Result.ok(fileList);
+    }
+    private File filesetcategory(File file,String extName){
+        if(Arrays.asList(video).contains(extName)){
+            file.setFileCategory(1);
+            return file;
+        }
+        if(Arrays.asList(music).contains(extName)){
+            file.setFileCategory(2);
+            return file;
+        }
+        if(Arrays.asList(image).contains(extName)){
+            file.setFileCategory(3);
+            return file;
+        }
+        if(Arrays.asList(word).contains(extName)||Arrays.asList(pdf).contains(extName)||Arrays.asList(excel).contains(extName)){
+            file.setFileCategory(4);
+            return file;
+        }
+        file.setFileCategory(5);
+        return file;
     }
 }
